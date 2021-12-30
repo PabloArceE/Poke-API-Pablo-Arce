14f476b19714cc8c66a6a4530eca7ab34be93a68
PabloArceE <arcepablo07@gmail.com>
2021-12-29 21:40:33 -0300

    29/12 21:40

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './style.css';
-import {} from './classes';
-import {} from './js/componentes';
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchPokemon(1)
+})
+
+const fetchPokemon = async(id) => {
+    try {
+        
+        const res  = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const data = await res.json()
+
+        const pokemon = {
+            foto: data.sprites.other.dream_world.front_default
+        }
+        
+        verPokemon();
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const verPokemon = (pokemon) => {
+
+}
+
