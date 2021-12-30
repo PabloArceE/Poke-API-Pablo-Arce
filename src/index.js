bf2ea6ff47f4611caf1e0b6ca2fd1c6548ebea41
PabloArceE <arcepablo07@gmail.com>
2021-12-30 00:42:38 -0300

    30/12 00:42

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import './style.css';
-
 document.addEventListener('DOMContentLoaded', () => {
     fetchPokemon(1)
 })
 
 const fetchPokemon = async(id) => {
+
     try {
         
         const res  = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         const data = await res.json()
 
-        const pokemon = {
-            foto: data.sprites.other.dream_world.front_default
+        console.log(data)
+
+        let pokemon = {
+            foto       : data.sprites.other.dream_world.front_default,
+            nombre     : data.name,
+            tipo       : data.types[0].type.name,
+            peso       : data.weight,
+            habilidades: data.abilities[0].ability.name
         }
         
-        verPokemon();
+        verPokemon(pokemon)
 
     } catch (err) {
         console.log(err)
@@ -22,6 +27,23 @@ const fetchPokemon = async(id) => {
 }
 
 const verPokemon = (pokemon) => {
+
+    console.log(pokemon)
+
+    const main     = document.querySelector('.main')
+    const template = document.querySelector('#template').content
+    const clone    = template.cloneNode(true)
+    const fragment = document.createDocumentFragment()
+
+    clone.querySelector('.foto-pokemon').setAttribute('src', pokemon.foto)
+    clone.querySelector('.pokemon-nombre').innerHTML      = `Nombre: ${pokemon.nombre}`
+    clone.querySelector('.pokemon-tipo').innerHTML        = `Tipo: ${pokemon.tipo}`
+    clone.querySelector('.pokemon-peso').innerHTML        = `Peso: ${pokemon.peso}`
+    clone.querySelector('.pokemon-habilidades').innerHTML = `Habilidades: ${pokemon.habilidades}`
+    
+
+    fragment.appendChild(clone)
+    main.appendChild(fragment)
 
 }
 
